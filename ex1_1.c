476a8c285b7c3951ed5ef0535cb9e241e23db164
Julien Noel <julien.leon99@gmail.com>
2020-08-10 10:14:11 +0200

    initial commit

---
+++ ex1_1.c
@@ -0,0 +1,44 @@
+/*
+
+1. 
+Ce sont des instruction qui permettent de tester si une condition est vraie ou non.
+il y a :
+if
+if ... else
+switch
+for
+while
+
+2.
+switch est pratique pour tester plusieur résultat possible sur une condition.
+Quand à if il sert plus à verifier si la condition est vrai.
+
+switch(couleur) {
+    case rouge :
+    printf("rouge\n");break;
+
+
+    case bleu :
+    printf("bleu\n");break;
+
+    case jaune :
+    printf("jaune\n");break;
+
+    default:
+    printf("ni rouge ni bleu ni jaune\n");break;
+
+}
+
+if(strcmp(couleur, "rouge")){
+    printf("c'est la couleur rouge");
+}
+
+3.
+C?X:Y
+
+si la condition C est vrai
+    faire X
+sinon
+    faire Y
+
+*/
